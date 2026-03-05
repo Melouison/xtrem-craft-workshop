dba48bcb6c49a05827ffc7f774d357d122fb9053
Louison Menuet <louison.menuet@gmail.com>
2026-03-05 10:59:21 +0100

    rename test

--- python/tests/test_bank.py
+++ python/tests/test_bank.py
@@ -7,18 +7,18 @@ from python.src.missing_exchange_rate_error import MissingExchangeRateError
 
 
 class TestBank:
-    def test_convert_euro_to_usd_returns_float(self):
+    def test_should_return_float_when_convertng_euro_to_usd(self):
         assert Bank.create(Currency.EUR, Currency.USD, 1.2).convert(10, Currency.EUR, Currency.USD) == 12
 
-    def test_convert_euro_to_usd_returns_same_value(self):
+    def test_should_return_same_value_when_converting_currency_to_itself(self):
         assert Bank.create(Currency.EUR, Currency.USD, 1.2).convert(10, Currency.EUR, Currency.EUR) == 10
 
-    def test_convert_with_missing_exchange_rate_throws_exception(self):
+    def test_should_not_convert_between_different_currencies_when_exchange_rate_is_missing(self):
         with pytest.raises(MissingExchangeRateError) as error:
             Bank.create(Currency.EUR, Currency.USD, 1.2).convert(10, Currency.EUR, Currency.KRW)
         assert str(error.value) == "EUR->KRW"
 
-    def test_convert_with_different_exchange_rate_returns_different_floats(self):
+    def test_should_returns_differents_floats_whenconverintngf_different_exchange_rate(self):
         bank: Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
         assert bank.convert(10, Currency.EUR, Currency.USD) == 12
 
